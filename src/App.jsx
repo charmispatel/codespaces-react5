467da1bdc6d7f53157cd383b4fd460a5791bf638
charmispatel <108417511+charmispatel@users.noreply.github.com>
2024-12-04 15:39:42 +0000

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn and go
           </a>
         </p>
       </header>
